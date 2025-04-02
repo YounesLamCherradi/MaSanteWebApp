5b3b34a76b9a0fed0b5b97a80e332876810deaf7
fsafiri <fsafiri171@gmail.com>
2025-04-02 22:04:20 +0000

    Update urls.py

--- masante/urls.py
+++ masante/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path('', include('masanteapp.urls')),
     path('__debug__/', include(debug_toolbar.urls))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
