c51900e132f58788d97e8851e67f83e41445e47b
fsafiri <fsafiri171@gmail.com>
2025-04-02 22:10:18 +0000

    Update urls.py

--- masante/urls.py
+++ masante/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from django.conf.urls.static import static
+
 
 import masanteapp
 
